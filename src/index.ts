d3357abc01f6c86a97de8845fcd69864958058fc
Unknown <bdistin@gmail.com>
2019-03-22 14:23:24 -0500

    duh

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 // Copyright (c) 2018-2019 KlasaCommunityPlugins. All rights reserved. MIT license.
 import { Client as KlasaClient } from 'klasa';
 
-import { default as Client } from './lib/Client';
-export { Client };
+export { default as Client } from './lib/Client';
 // @ts-ignore
 exports[KlasaClient.plugin] = Client[KlasaClient.plugin];
